7c3377baa88e10d1523ec7c714295aa0c2c10162
StudentPP1 <mishamakytonin@gmail.com>
2024-07-24 17:04:18 +0300

    change version of jdk to 21 in Dockerfile and add SpringBootServletInitializer

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM  maven:3-openjdk-18 AS build
+FROM  maven:3.9.8-eclipse-temurin-21 AS build
 COPY . .
 RUN mvn clean package -DskipTests
 
-FROM openjdk:18
+FROM openjdk:21
 COPY --from=build /target/backend-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/backend/ToDoListBackendApplication.java
+++ src/main/java/com/example/backend/ToDoListBackendApplication.java
@@ -2,11 +2,12 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class ToDoListBackendApplication {
+public class ToDoListBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListBackendApplication.class, args);
